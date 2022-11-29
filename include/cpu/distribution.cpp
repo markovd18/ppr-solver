c1fcf0603895d225cfdf8978ba38a7039980e9b8
David Markov <Markov.David@seznam.cz>
2022-11-29 18:34:40 +0100

    readme, removed rubish

--- include/cpu/distribution.cpp
+++
@@ -1,9 +0,0 @@
-#include "distribution.h"
-
-std::string get_distribution_name(const EDistribution distribution) {
-    return std::string();
-}
-
-double get_distribution_kurtosis(const EDistribution distribution, const double mean) {
-    return 0.0;
-}
